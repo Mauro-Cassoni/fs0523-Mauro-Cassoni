1583ce549affa75442c60a687485d28e6d173022
Mauro-Cassoni <mauro.cassoni.94@gmail.com>
2023-10-11 16:46:39 +0200

    esercizio task manager

---
+++ m1/s3/g3/assets/script/script.js
@@ -0,0 +1,30 @@
+let button = document.querySelector('#button');
+
+button.addEventListener("click",function(){
+    
+    let lista = document.querySelector('#lista');
+    let ListaOk = document.querySelector('#listaOk');
+    let testo = document.querySelector('#testo');
+
+    if(!testo.value) return;
+
+    let li = document.createElement("li");
+    let del = document.createElement("span");
+    li.classList.add('list-item');
+    del.classList.add('del-item');
+    li.innerText = testo.value;
+    del.innerText = 'Elimina';
+    
+    li.addEventListener('click',function(){
+        li.classList.toggle('ok');
+    })
+
+    del.addEventListener('click',function(){
+        li.remove()
+        del.remove()
+    })
+
+
+    lista.append(li, del);
+    testo.value = '';
+});
