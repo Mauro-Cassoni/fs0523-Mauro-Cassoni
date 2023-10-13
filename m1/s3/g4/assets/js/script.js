bf1d0b5766ab037a8271fa6862a53ca806155cb7
Mauro-Cassoni <mauro.cassoni.94@gmail.com>
2023-10-13 12:30:14 +0200

    eserizi js fino a n.9

--- m1/s3/g4/assets/js/script.js
+++ m1/s3/g4/assets/js/script.js
@@ -2,7 +2,6 @@ let tabellone = document.querySelector('.tabellone');
 let bottoneNumero = document.querySelector('.bottoneNumero');
 let cartelle = document.querySelector('.cartelle')
 let bottoneCartella = document.querySelector('.bottoneCartella')
-let numeroC = cartelle.querySelectorAll('.numero-cartella');
 
 for(i = 1; i <= 76; i++){
     let numero = document.createElement("div");
@@ -12,6 +11,7 @@ for(i = 1; i <= 76; i++){
 }
 
 bottoneNumero.addEventListener('click', function(){
+let numeroC = cartelle.querySelectorAll('.numero-cartella');
     let num = Math.floor(Math.random() * 76) + 1;
     for(i = 1; i <= 76; i++){
         let numeroT = tabellone.querySelectorAll('.numero-tabellone')[i];
