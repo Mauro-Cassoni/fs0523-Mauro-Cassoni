2bc106d2a2dea5f77caa453ead7ca32702bc1c02
Mauro-Cassoni <mauro.cassoni.94@gmail.com>
2023-11-09 17:13:52 +0100

    primo esercizio API

---
+++ m2/s3/g4/assets/js/script.js
@@ -0,0 +1,46 @@
+let query = 'japan';
+
+let image = async (query) => {
+    const res = await fetch("https://api.pexels.com/v1/search?query=" + query, {
+        method: "GET",
+        headers: {
+            "Authorization": "F2tJ0wXPIwjrBwpi91zhoVkn07Trdc2mB5sCF8o6UGEcvp4vnZ3RROEs"
+        }
+    });
+
+    const body = await res.json();
+
+    if (Array.isArray(body.photos)) {
+        body.photos.forEach(photo => {
+            new Card(photo.src.large);
+        });
+    } else {
+        console.error('I dati dell\'API non contengono un array di foto.');
+    }
+
+    console.log(body);
+}
+
+image(query);
+
+class Card {
+    constructor(img) {
+        this.img = img;
+        this.cloneTemplate();
+        this.compileCard();
+    }
+
+    cloneTemplate() {
+        const template = document.getElementsByTagName("template")[0].content;
+        const clone = template.cloneNode(true);
+        const cloneContainer = document.getElementById("containerTemplate");
+        cloneContainer.appendChild(clone);
+    }
+
+    compileCard() {
+        const cards = document.querySelectorAll('.card');
+        const lastCard = cards[cards.length - 1];
+        const immagine = lastCard.querySelector('img');
+        immagine.src = this.img;
+    }
+}
