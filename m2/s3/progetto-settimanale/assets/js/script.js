faa7b00390027b4c92779837e0cbd4b69bf0fa7f
Mauro-Cassoni <mauro.cassoni.94@gmail.com>
2023-11-10 13:02:01 +0100

    fine pagina creazione

--- m2/s3/progetto-settimanale/assets/js/script.js
+++ m2/s3/progetto-settimanale/assets/js/script.js
@@ -1,53 +1,60 @@
-const link = 'https://striveschool-api.herokuapp.com/api/product/'
-const key = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTRkZTA0MTI1NGU4ODAwMTgzZjE4NjUiLCJpYXQiOjE2OTk2MDI0OTcsImV4cCI6MTcwMDgxMjA5N30.uhqYWIoMloYMqF8_ehiAfaDV4ulDZ37VcyxV8VQ6Yoo"
+const LINK = 'https://striveschool-api.herokuapp.com/api/product/'
+const KEY = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTRkZTA0MTI1NGU4ODAwMTgzZjE4NjUiLCJpYXQiOjE2OTk2MDI0OTcsImV4cCI6MTcwMDgxMjA5N30.uhqYWIoMloYMqF8_ehiAfaDV4ulDZ37VcyxV8VQ6Yoo"
 
-let saveBtn = document.querySelector('.save');
+let addBtn = document.querySelector('.addButton');
 
-saveBtn.addEventListener('click', function (e) {
+addBtn.addEventListener('click', function (e) {
 
-e.preventDefault();
+    e.preventDefault();
 
-//PRENDO TUTTI I VALORI DEL FORM
-let name = document.querySelector('#gusto').value;
-let description = Number(document.querySelector('#prezzo').value);
-let brand = Boolean(document.querySelector('#disponibile').value);
-let imageUrl = Boolean(document.querySelector('#disponibile').value);
-let price = Boolean(document.querySelector('#disponibile').value);
+    //PRENDO TUTTI I VALORI DEL FORM
+    let name = document.querySelector('#validationCustom01').value;
+    let description = document.querySelector('#validationCustom03').value;
+    let brand = document.querySelector('#validationCustom02').value;
+    let imageUrl = document.querySelector('#validationCustom04').value;
+    let price = Number(document.querySelector('#validationCustom05').value);
 
-//li uso per costruire un oggetto da inviare al server
-let newPhone = {
-name,
-description,
-brand,
-imageUrl,
-price
-}
+    //li uso per costruire un oggetto da inviare al server
+    class Phone {
+        constructor(name, description, brand, imageUrl, price) {
+            this.name = name;
+            this.description = description;
+            this.brand = brand;
+            this.imageUrl = imageUrl;
+            this.price = price;
+        }
+    }
 
-//faccio la fetch che invia i dati al server
-fetch('https://striveschool-api.herokuapp.com/api/product/', {
-method: 'POST',
-headers: {
-"Authorization": key,
-'Content-Type': 'application/json'
-},
-body: JSON.stringify(newPhone)//i dati inviati vengono convertiti in json
+    //faccio la fetch che invia i dati al server
+    fetch(LINK, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            "Authorization": KEY,
+        },
+        body: JSON.stringify(new Phone(name, description, brand, imageUrl, price))//i dati inviati vengono convertiti in json
+    })
+        .then(res => res.json())
+        .then(phones => {
+
+            
+
+
+            //ho deciso di fare un redirect se la pizza viene creata correttamente, potevo anche mostrare un avviso
+            // location.href = '/index.html';
+
+        })
+
+
 })
-.then(res => res.json())
-.then(phones => {
-
-//ho deciso di fare un redirect se la pizza viene creata correttamente, potevo anche mostrare un avviso
-location.href = '/lista.html'
-
-})
-
-
-})
 
 
 
 
-fetch(link, {
-headers: {
-"Authorization": key
-}
-})
+// fetch(link, {
+//     headers: {
+//         "Authorization": key
+//     }
+// })
+
+
