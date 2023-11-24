2fcae3ab5be7ecdc29e2da4a959af06bdc22950c
Mauro-Cassoni <mauro.cassoni.94@gmail.com>
2023-11-24 16:18:56 +0100

    last commit

--- m3/s1/progetto-settimanale/js/script.js
+++ m3/s1/progetto-settimanale/js/script.js
@@ -15,41 +15,91 @@ class Smartphone {
         return this.numeroChiamate;
     }
     chiamata(min) {
-        this.numeroChiamate += 1;
+        this.numeroChiamate++;
         this.carica -= (min * this.costoMinuto);
     }
     azzeraChiamate() {
         this.numeroChiamate = 0;
     }
 }
-let EmaSmartphone = new Smartphone();
-EmaSmartphone.ricarica(10);
-console.log(EmaSmartphone.numero404());
-console.log(EmaSmartphone.getNumeroChiamate());
-EmaSmartphone.chiamata(5);
-EmaSmartphone.chiamata(20);
-console.log(EmaSmartphone.getNumeroChiamate());
-console.log(EmaSmartphone.numero404());
-EmaSmartphone.azzeraChiamate();
-console.log(EmaSmartphone.getNumeroChiamate());
-let EmiSmartphone = new Smartphone();
-EmiSmartphone.ricarica(100);
-console.log(EmiSmartphone.numero404());
-console.log(EmiSmartphone.getNumeroChiamate());
-EmiSmartphone.chiamata(15);
-console.log(EmiSmartphone.getNumeroChiamate());
-console.log(EmiSmartphone.numero404());
-EmiSmartphone.azzeraChiamate();
-console.log(EmiSmartphone.getNumeroChiamate());
-let MauroSmartphone = new Smartphone();
-MauroSmartphone.ricarica(15);
-console.log(MauroSmartphone.numero404());
-console.log(MauroSmartphone.getNumeroChiamate());
-MauroSmartphone.chiamata(5);
-MauroSmartphone.chiamata(12);
-MauroSmartphone.chiamata(7);
-console.log(MauroSmartphone.getNumeroChiamate());
-console.log(MauroSmartphone.numero404());
-MauroSmartphone.azzeraChiamate();
-console.log(MauroSmartphone.getNumeroChiamate());
+function controllaSmartphone(smartphone, ricarica, minuti1, minuti2 = 0) {
+    smartphone.ricarica(ricarica);
+    console.log(smartphone.numero404());
+    console.log(smartphone.getNumeroChiamate());
+    smartphone.chiamata(minuti1);
+    smartphone.chiamata(minuti2);
+    console.log(smartphone.getNumeroChiamate());
+    console.log(smartphone.numero404());
+    smartphone.azzeraChiamate();
+    console.log(smartphone.getNumeroChiamate());
+}
+let emaSmartphone = new Smartphone();
+controllaSmartphone(emaSmartphone, 10, 5, 20);
+let emiSmartphone = new Smartphone();
+controllaSmartphone(emiSmartphone, 100, 15, 3);
+let mauroSmartphone = new Smartphone();
+controllaSmartphone(mauroSmartphone, 15, 5, 12);
+class ExtraSmartphone {
+    constructor(carica = 0, numeroChiamate = 0, costoMinuto = 0.20, registroChiamate, partenzaID = 1) {
+        this.carica = carica;
+        this.numeroChiamate = numeroChiamate;
+        this.costoMinuto = costoMinuto;
+        this.registroChiamate = registroChiamate;
+        this.partenzaID = partenzaID;
+    }
+    ricarica(euro) {
+        this.carica += euro;
+    }
+    numero404() {
+        return `Il credito residuo è di ${this.carica.toFixed(2)}€`;
+    }
+    getNumeroChiamate() {
+        return this.numeroChiamate;
+    }
+    chiamata(min) {
+        const timestamp = Date.now();
+        const dataEora = new Date(timestamp);
+        const anno = dataEora.getFullYear();
+        const mese = dataEora.getMonth() + 1;
+        const giorno = dataEora.getDate();
+        const ore = dataEora.getHours();
+        const minuti = dataEora.getMinutes();
+        const secondi = dataEora.getSeconds();
+        const dataOra = `Chiamata effettuata alle ore ${ore}:${minuti}:${secondi} del ${giorno}-${mese}-${anno}`;
+        let id = this.partenzaID++;
+        let chiamata = { id, min, dataOra, timestamp };
+        if (!this.registroChiamate) {
+            this.registroChiamate = [];
+        }
+        this.registroChiamate.push(chiamata);
+        this.numeroChiamate++;
+        this.carica -= (min * this.costoMinuto);
+    }
+    azzeraChiamate() {
+        this.numeroChiamate = 0;
+    }
+    mostraRegistroChiamate() {
+        return this.registroChiamate || [];
+    }
+    filtraChiamatePerDataOra(filtroOra, filtroGiorno, filtroMese, filtroAnno) {
+        if (!this.registroChiamate) {
+            return [];
+        }
+        return this.registroChiamate.filter(chiamata => {
+            const dataEora = new Date(chiamata.timestamp);
+            const anno = dataEora.getFullYear();
+            const mese = dataEora.getMonth() + 1;
+            const giorno = dataEora.getDate();
+            const ora = dataEora.getHours();
+            return ((!filtroAnno || anno === filtroAnno) &&
+                (!filtroMese || mese === filtroMese) &&
+                (!filtroGiorno || giorno === filtroGiorno) &&
+                (!filtroOra || ora === filtroOra));
+        });
+    }
+}
+let micheleExtraSmartphone = new ExtraSmartphone();
+controllaSmartphone(micheleExtraSmartphone, 250, 20, 35);
+console.log(micheleExtraSmartphone.registroChiamate);
+console.log(micheleExtraSmartphone.filtraChiamatePerDataOra(16, 24, 11, 2023));
 //# sourceMappingURL=script.js.map
--- m3/s1/progetto-settimanale/ts/script.ts
+++ m3/s1/progetto-settimanale/ts/script.ts
@@ -12,12 +12,13 @@ interface ISim {
 
 }
 
+
 class Smartphone implements ISim {
 
     constructor(
         public carica: number = 0,
         public numeroChiamate: number = 0,
-        public costoMinuto: number = 0.2
+        public costoMinuto: number = 0.2,
     ) { }
 
     ricarica(euro: number): void {
@@ -30,7 +31,7 @@ class Smartphone implements ISim {
         return this.numeroChiamate;
     }
     chiamata(min: number): void {
-        this.numeroChiamate += 1;
+        this.numeroChiamate++;
         this.carica -= (min * this.costoMinuto);
     }
     azzeraChiamate(): void {
@@ -39,42 +40,105 @@ class Smartphone implements ISim {
 
 }
 
-let EmaSmartphone = new Smartphone()
+function controllaSmartphone(smartphone: Smartphone, ricarica: number, minuti1: number, minuti2: number = 0): void {
+    smartphone.ricarica(ricarica);
+    console.log(smartphone.numero404());
+    console.log(smartphone.getNumeroChiamate());
+    smartphone.chiamata(minuti1);
+    smartphone.chiamata(minuti2);
+    console.log(smartphone.getNumeroChiamate());
+    console.log(smartphone.numero404());
+    smartphone.azzeraChiamate();
+    console.log(smartphone.getNumeroChiamate());
+}
 
-EmaSmartphone.ricarica(10);
-console.log(EmaSmartphone.numero404());
-console.log(EmaSmartphone.getNumeroChiamate());
-EmaSmartphone.chiamata(5);
-EmaSmartphone.chiamata(20);
-console.log(EmaSmartphone.getNumeroChiamate());
-console.log(EmaSmartphone.numero404());
-EmaSmartphone.azzeraChiamate()
-console.log(EmaSmartphone.getNumeroChiamate());
-
-
-let EmiSmartphone = new Smartphone()
-
-EmiSmartphone.ricarica(100);
-console.log(EmiSmartphone.numero404());
-console.log(EmiSmartphone.getNumeroChiamate());
-EmiSmartphone.chiamata(15);
-console.log(EmiSmartphone.getNumeroChiamate());
-console.log(EmiSmartphone.numero404());
-EmiSmartphone.azzeraChiamate()
-console.log(EmiSmartphone.getNumeroChiamate());
+let emaSmartphone: Smartphone = new Smartphone();
+controllaSmartphone(emaSmartphone,10,5,20);
 
 
-let MauroSmartphone = new Smartphone()
+let emiSmartphone: Smartphone = new Smartphone();
+controllaSmartphone(emiSmartphone,100,15,3);
 
-MauroSmartphone.ricarica(15);
-console.log(MauroSmartphone.numero404());
-console.log(MauroSmartphone.getNumeroChiamate());
-MauroSmartphone.chiamata(5);
-MauroSmartphone.chiamata(12);
-MauroSmartphone.chiamata(7);
-console.log(MauroSmartphone.getNumeroChiamate());
-console.log(MauroSmartphone.numero404());
-MauroSmartphone.azzeraChiamate()
-console.log(MauroSmartphone.getNumeroChiamate());
+
+let mauroSmartphone: Smartphone = new Smartphone();
+controllaSmartphone(mauroSmartphone,15,5,12);
+
+
+
+class ExtraSmartphone implements ISim {
+    constructor(
+        public carica: number = 0,
+        public numeroChiamate: number = 0,
+        public costoMinuto: number = 0.20,
+        public registroChiamate?: { id: number, min: number, dataOra: string, timestamp:number }[],
+        private partenzaID: number = 1
+    ) { }
+
+    ricarica(euro: number): void {
+        this.carica += euro;
+    }
+    numero404(): string {
+        return `Il credito residuo è di ${this.carica.toFixed(2)}€`;
+    }
+    getNumeroChiamate(): number {
+        return this.numeroChiamate;
+    }
+    chiamata(min: number): void {
+        const timestamp:number = Date.now();
+        const dataEora = new Date(timestamp);
+        
+        const anno:number = dataEora.getFullYear();
+        const mese:number = dataEora.getMonth() + 1;
+        const giorno:number = dataEora.getDate();
+        const ore:number = dataEora.getHours();
+        const minuti:number = dataEora.getMinutes();
+        const secondi:number = dataEora.getSeconds();
+        
+        const dataOra = `Chiamata effettuata alle ore ${ore}:${minuti}:${secondi} del ${giorno}-${mese}-${anno}`;
+        
+        let id = this.partenzaID++;
+        let chiamata = { id, min, dataOra, timestamp}
+        if (!this.registroChiamate) {
+            this.registroChiamate = [];
+        }
+        this.registroChiamate.push(chiamata);
+        this.numeroChiamate++;
+        this.carica -= (min * this.costoMinuto);
+    }
+    azzeraChiamate(): void {
+        this.numeroChiamate = 0;
+    }
+
+    mostraRegistroChiamate() {
+        return this.registroChiamate || [];
+    }
+    filtraChiamatePerDataOra(filtroOra?: number, filtroGiorno?: number, filtroMese?: number, filtroAnno?: number): any[] {
+        if (!this.registroChiamate) {
+            return [];
+        }
+    
+        return this.registroChiamate.filter(chiamata => {
+            const dataEora = new Date(chiamata.timestamp);
+            const anno = dataEora.getFullYear();
+            const mese = dataEora.getMonth() + 1;
+            const giorno = dataEora.getDate();
+            const ora = dataEora.getHours();
+    
+            return (
+                (!filtroAnno || anno === filtroAnno) &&
+                (!filtroMese || mese === filtroMese) &&
+                (!filtroGiorno || giorno === filtroGiorno) &&
+                (!filtroOra || ora === filtroOra)
+            );
+        });
+    }
+
+}
+
+let micheleExtraSmartphone: ExtraSmartphone = new ExtraSmartphone();
+controllaSmartphone(micheleExtraSmartphone,250,20,35);
+
+console.log(micheleExtraSmartphone.registroChiamate);
+console.log(micheleExtraSmartphone.filtraChiamatePerDataOra(16,24,11,2023));
 
 
